505bfe1b70dc3385b2fe085cb503083e46b659eb
Md-Hadiuzzaman-Rifat <hadirifat100@gmail.com>
2024-02-14 06:09:42 +0600

    will work on loading button

--- src/components/Discount/DiscountSell.js
+++ src/components/Discount/DiscountSell.js
@@ -1,11 +1,10 @@
 import React from "react";
 import SingleProduct from "../SingleProduct/SingleProduct";
-import "./DiscountSell.scss"
+import "./DiscountSell.scss";
 import { useGetProductsQuery } from "../../features/product/productApi";
 
-
 const DiscountSell = () => {
-  const { data=[], error: isError, isLoading } = useGetProductsQuery();
+  const { data = [], error: isError, isLoading } = useGetProductsQuery();
 
   return (
     <div className="discountSell">
@@ -13,12 +12,20 @@ const DiscountSell = () => {
         <h2>Discount Product</h2>
         <div className="discount_product">
           {isLoading && "Loading..."}
-          {!isLoading && data?.length>0 && data
-            .filter(item=>!item.featured)
-            .map(item => (
-              <SingleProduct className="discount_product_item" item={item} key={item._id}></SingleProduct>
-            ))
-          }
+          {!isLoading &&
+            data?.length > 0 &&
+            data
+              .filter((item) => !item.featured)
+              .map((item) => (
+                <SingleProduct
+                  className="discount_product_item"
+                  item={item}
+                  key={item._id}
+                ></SingleProduct>
+              ))}
+        </div>
+        <div className="load-button">
+          <button>Load More...</button>
         </div>
       </div>
     </div>
