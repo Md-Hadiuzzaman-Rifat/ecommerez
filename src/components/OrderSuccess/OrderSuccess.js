17476752021f6e64d0180ced96ad2210d9bc56f0
Md-Hadiuzzaman-Rifat <hadirifat100@gmail.com>
2024-03-01 04:25:13 +0600

    done

--- src/components/OrderSuccess/OrderSuccess.js
+++ src/components/OrderSuccess/OrderSuccess.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import "./OrderSuccess.scss"
+import React from "react";
+import "./OrderSuccess.scss";
 
 const OrderSuccess = () => {
-    return (
-        <div className='OrderSuccess'>
-            <div className="container">
+  return (
+    <div className="OrderSuccess">
+      <div className="container">
         <i className="fas fa-check-circle"></i>
         <h1>Thank you for your order!</h1>
-        <p>Your order has been received and is being processed. You will receive an email confirmation shortly.</p>
-        <a href="/" className="button">Continue Shopping</a>
+        <p>
+          Your order has been received and is being processed. You will receive
+          an email confirmation shortly.
+        </p>
+        <a href="/" className="button">
+          Continue Shopping
+        </a>
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default OrderSuccess;
